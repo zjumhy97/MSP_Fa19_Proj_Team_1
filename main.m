3d9f1acec2b5a2bdd89c0f9d1f65fa05acfac069
zjumhy97 <945229411@qq.com>
2019-12-10 14:49:59 +0800

    create main.m

---
+++ main.m
@@ -0,0 +1,9 @@
+%% main.m
+%  Modern Signal Processing (2019 Fall)
+%  Project: Eye image Segmentation (color image)
+%  Director: Prof. Xiaoying Tang
+%  Date: 2019/12/10
+%  Author: Team 1
+%  Github: https://github.com/zjumhy97/MSP_Fa19_Proj_Team_1
+
+%% 
