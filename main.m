b39be0b61d15f2d04f0735c8eba82dd9479f4e86
zjumhy97 <945229411@qq.com>
2019-12-15 14:05:40 +0800

    read data & rgb2hsv

--- main.m
+++ main.m
@@ -1,10 +1,12 @@
-%% main.m
+%**************************************************************************
+%  main.m
 %  Modern Signal Processing (2019 Fall)
 %  Project: Eye image Segmentation (color image)
 %  Director: Prof. Xiaoying Tang
 %  Date: 2019/12/10
 %  Author: Team 1
 %  Github: https://github.com/zjumhy97/MSP_Fa19_Proj_Team_1
+%**************************************************************************
 
 %% Input Figure
 % the original jpg is in RGB, HSV information may need.
